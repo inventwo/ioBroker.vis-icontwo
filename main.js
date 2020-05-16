7e0a79af62ab6ec5533d6d0ac9149e47fff636f6
jkvarel <jannis@kammann.cc>
2020-05-16 14:43:55 +0200

    ### 0.1.0 - initial release

---
+++ main.js
@@ -0,0 +1,38 @@
+"use strict";
+
+/*
+ * Created with @ioBroker/create-adapter v1.21.1
+ */
+
+// The adapter-core module gives you access to the core ioBroker functions
+// you need to create an adapter
+const utils = require("@iobroker/adapter-core");
+
+// Load  your  modules here, e.g.:
+// const fs = require("fs");
+
+class visIcontwo extends utils.Adapter {
+
+	/**
+	 * @param {Partial<ioBroker.AdapterOptions>} [options={}]
+	 */
+	constructor(options) {
+		super({
+			...options,
+			name: "vis-icontwo",
+		});
+	}
+
+}
+
+// @ts-ignore parent is a valid property on module
+if (module.parent) {
+	// Export the constructor in compact mode
+	/**
+	 * @param {Partial<ioBroker.AdapterOptions>} [options={}]
+	 */
+	module.exports = (options) => new visIcontwo(options);
+} else {
+	// otherwise start the instance directly
+	new visIcontwo();
+}
