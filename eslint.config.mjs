313d16e9a089b0e25e9f4e25b4ef4cf6cc87dd99
skvarel <skvarel@inventwo.com>
2026-05-27 16:10:06 +0200

    Bugfix

---
+++ eslint.config.mjs
@@ -0,0 +1,19 @@
+import config from '@iobroker/eslint-config';
+
+export default [
+    ...config,
+    {
+        languageOptions: {
+            parserOptions: {
+                projectService: {
+                    allowDefaultProject: ['*.js', '*.mjs'],
+                },
+                tsconfigRootDir: import.meta.dirname,
+            },
+        },
+    },
+    {
+        // specify files to exclude from linting here
+        ignores: ['admin/', 'www/', 'img/', 'node_modules/'],
+    },
+];
